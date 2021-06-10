bc7f493cd888f8a2684b497e34fbb93af709a6a8
Ismael Delgado <justridelow@gmail.com>
2021-06-10 15:53:18 -0500

    updating report options

--- src/app/data-list.service.ts
+++ src/app/data-list.service.ts
@@ -2,16 +2,12 @@
 
 export class DataListService{
   todo = [
-    'Service',
-    'Get to work',
-    'Pick up groceries',
-    'Go home',
-    'Fall asleep',
-    'Get up',
-    'Brush teeth',
-    'Take a shower',
-    'Check e-mail',
-    'Walk dog'
+    'Pan',
+    'NetworkReferenceId',
+    'transanctionId',
+    'totalTransanctionAmount',
+    'IssuerAmount',
+    'AcquirerAmount'
   ];
 
   public getList():string[]{
